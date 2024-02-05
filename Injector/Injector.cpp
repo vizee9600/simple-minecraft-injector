ac63e9f43b77d69638dc3994af1d80d2d6e02766
vizee <71572483+vizee9600@users.noreply.github.com>
2024-02-06 00:11:45 +0100

    DLL Selection

--- Injector/Injector.cpp
+++ Injector/Injector.cpp
@@ -4,9 +4,28 @@
 #include <iostream>
 using namespace std;
 
-const char* dll_path = "path to dll";
+void rndmTitle() {
+	constexpr int length = 15;
+	const auto characters = TEXT("abcdefghi9182345jklmnopqrstuv211935960473wxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+	TCHAR title[length + 1]{};
+
+	for (int j = 0; j != length; j++)
+	{
+		title[j] += characters[rand() % 80];
+	}
+
+	SetConsoleTitleW(title);
+}
 
 int main(int argc, char** argv) {
+
+	string path;
+
+	cout << "Path to .DLL: " << endl;
+
+	cin >> path;
+
+	const char* dll_path = path.c_str();
 
 	HANDLE snapshot = 0;
 	PROCESSENTRY32 pe32 = { 0 };
@@ -21,7 +40,7 @@ int main(int argc, char** argv) {
 	do {
 		if (wcscmp(pe32.szExeFile, L"javaw.exe") == 0) {
 			HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, true, pe32.th32ProcessID);
-			std::cout << "injected";
+			
 			void* lpBaseAddress = VirtualAllocEx(process, NULL, strlen(dll_path) + 1, MEM_COMMIT, PAGE_READWRITE);
 
 			WriteProcessMemory(process, lpBaseAddress, dll_path, strlen(dll_path) + 1, NULL);
@@ -29,6 +48,7 @@ int main(int argc, char** argv) {
 			HMODULE kernel32base = GetModuleHandle(L"kernel32.dll");
 			HANDLE thread = CreateRemoteThread(process, NULL, 0, (LPTHREAD_START_ROUTINE)GetProcAddress(kernel32base, "LoadLibraryA"), lpBaseAddress, 0, NULL);
 
+			std::cout << "injected";
 
 			WaitForSingleObject(thread, INFINITE);
 			GetExitCodeThread(thread, &exitCode);
